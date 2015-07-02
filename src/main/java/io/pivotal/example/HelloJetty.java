a182f85037b81afa706ab8a13dcd55f8a469e3ac
Andrew Ripka <aripka@pivotal.io>
2015-07-02 14:42:06 -0500

    change port to bey environment variable

--- src/main/java/io/pivotal/example/HelloJetty.java
+++ src/main/java/io/pivotal/example/HelloJetty.java
@@ -4,12 +4,19 @@ import org.eclipse.jetty.server.Server;
 
 public class HelloJetty {
 
-	public static void main(String[] args) throws Exception{
-		 	Server server = new Server(8080);
-	        server.setHandler(new HelloWorld());
-	 
-	        server.start();
-	        server.join();
+	public static void main(String[] args) throws Exception {
+
+		String port = System.getenv("PORT");
+
+		if (port == null || port.isEmpty()) {
+			port = "8080";
+		}
+
+		Server server = new Server(Integer.parseInt(port));
+		server.setHandler(new HelloWorld());
+
+		server.start();
+		server.join();
 	}
-	
+
 }
